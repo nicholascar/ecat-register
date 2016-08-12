cc9ce56c12d07710318d667f612cc8ad0413fd0c
Nicholas Car <nicholas.car@csiro.au>
2016-08-12 21:42:48 +1000

    services endpoint

--- routes.py
+++ routes.py
@@ -1,6 +1,5 @@
 import os.path
-import json
-from flask import Blueprint, Response, request, redirect, render_template, g, jsonify, render_template_string
+from flask import Blueprint, Response, request, render_template, render_template_string
 import settings
 import functions
 routes = Blueprint('routes', __name__)
@@ -109,5 +108,104 @@ def datasets():
         return render_template(
             'dataset-register.xml',
             dataset_uris=dataset_uris,
+            mimetype='application/xml'
+        )
+
+
+@routes.route('/service')
+def services():
+    return render_template('services.html')
+
+
+@routes.route('/service/')
+def services():
+    # produce error note if dataset index missing
+    if not os.path.isfile(settings.SERVICES_URIS_FILE):
+        missing_txt = render_template_string(
+            open('templates/missing.html', 'r').read(),
+            title='Services',
+            missing_file='services index'
+        )
+        return Response(
+            missing_txt,
+            status=500
+        )
+
+    # get the dataset URIs
+    services_uris = open(settings.SERVICES_URIS_FILE).readlines()
+
+    # list supported mime types
+    human_mimes = [
+        'text/html',
+        'text/plain'
+    ]
+    # RDF mime types
+    rdf_mimes = [
+        'text/turtle',
+        'text/n3',
+        'text/ntriples',
+        'application/rdf+xml',
+        'application/rdf+json',
+        'application/json-ld',
+        'application/json',
+        'application/rdf+xml'
+    ]
+    # XML
+    xml_mimes = [
+        'application/xml',
+        'text/xml'
+    ]
+    # find the best matched mime type
+    all_mimes = human_mimes + rdf_mimes + xml_mimes
+    best_mime = request.accept_mimetypes.best_match(all_mimes)
+    # human-readable
+    if best_mime in human_mimes:
+        if best_mime == 'text/html':
+            return render_template(
+                'service-register.html',
+                services_uris=services_uris,
+                mime='text/html'
+            )
+        elif best_mime == 'text/uri-list':
+            uri_list = render_template_string(
+                open('templates/service-register.uri_list', 'r').read(),
+                services_uris=services_uris
+            )
+            return Response(
+                uri_list,
+                mimetype='text/uri-list'
+            )
+        else:  # text
+            txt = render_template_string(
+                open('templates/service-register.txt', 'r').read(),
+                services_uris=services_uris
+            )
+            return Response(
+                txt,
+                mimetype='text/plain',
+                headers={'Content-Disposition': 'attachment; filename="services.txt"'}
+            )
+    # RDF
+    elif best_mime in rdf_mimes:
+        # make the RDF class for the items
+        dpn_service = 'http://purl.org/dpn#Service'
+
+        g = functions.uri_list_to_graph(services_uris, dpn_service)
+        # return RDF
+        if best_mime == 'application/rdf+json' or best_mime == 'application/json-ld' or best_mime == 'application/json':
+            return Response(g.serialize(format='json-ld'), mimetype=best_mime)
+        elif best_mime == 'text/nt':
+            return Response(g.serialize(format='nt'), mimetype=best_mime)
+        elif best_mime == 'text/n3':
+            return Response(g.serialize(format='n3'), mimetype=best_mime)
+        elif best_mime == 'applications/rdf+xml':
+            return Response(g.serialize(format='xml'), mimetype=best_mime)
+        else:  # if best_mime == 'text/turtle':
+            return Response(g.serialize(format='turtle'), mimetype=best_mime)
+    # XML
+    else:  # if best_mime in rdf_mimes:
+        return render_template(
+            'dataset-register.xml',
+            services_uris=services_uris,
             mimetype='application/xml'
         )
